af125931bc815e9c48d76a2312dd5b8a204e6919
copyniinja <killfyz0.o@gmail.com>
2026-03-01 16:54:22 +0600

    feat(db): implement prisma client

---
+++ src/prisma/client.ts
@@ -0,0 +1,19 @@
+import { Env } from "@/config/env";
+
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client";
+
+let prisma: PrismaClient | null = null;
+
+export function createPrisma(env: Env): PrismaClient {
+  if (prisma) return prisma;
+
+  const connectionString = `${env.DATABASE_URL}`;
+  const adapter = new PrismaPg({ connectionString });
+
+  prisma = new PrismaClient({
+    log: env.NODE_ENV === "development" ? ["query", "info", "warn"] : ["error"],
+    adapter,
+  });
+  return prisma;
+}
